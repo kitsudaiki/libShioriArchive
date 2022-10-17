aed1ad51133159778c45e3294f5bf8c204399f16
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-10-17 20:18:21 +0200

    updates for tagging 0.3.0

--- build.sh
+++ build.sh
@@ -64,31 +64,30 @@ function download_repo_github () {
 
 
 #-----------------------------------------------------------------------------------------------------------------
-
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_kitsune_lib_repo "libKitsunemimiCommon" "develop" 8
-get_required_kitsune_lib_repo "libKitsunemimiJson" "develop" 1
-get_required_kitsune_lib_repo "libKitsunemimiJinja2" "develop" 1
-get_required_kitsune_lib_repo "libKitsunemimiIni" "develop" 1
-get_required_kitsune_lib_repo "libKitsunemimiNetwork" "develop" 8
-get_required_kitsune_lib_repo "libKitsunemimiArgs" "delopel" 8
-get_required_kitsune_lib_repo "libKitsunemimiConfig" "develop" 8
-get_required_kitsune_lib_repo "libKitsunemimiCrypto" "develop" 8
-get_required_kitsune_lib_repo "libKitsunemimiJwt" "develop" 8
+get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.27.1" 8
+get_required_kitsune_lib_repo "libKitsunemimiJson" "v0.12.0" 1
+get_required_kitsune_lib_repo "libKitsunemimiJinja2" "v0.10.0" 1
+get_required_kitsune_lib_repo "libKitsunemimiIni" "v0.6.0" 1
+get_required_kitsune_lib_repo "libKitsunemimiNetwork" "v0.9.0" 8
+get_required_kitsune_lib_repo "libKitsunemimiArgs" "v0.5.0" 8
+get_required_kitsune_lib_repo "libKitsunemimiConfig" "v0.5.0" 8
+get_required_kitsune_lib_repo "libKitsunemimiCrypto" "v0.3.0" 8
+get_required_kitsune_lib_repo "libKitsunemimiJwt" "v0.5.1" 8
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_kitsune_lib_repo "libKitsunemimiSakuraNetwork" "develop" 8
-get_required_kitsune_lib_repo "libKitsunemimiSakuraLang" "develop" 1
+get_required_kitsune_lib_repo "libKitsunemimiSakuraNetwork" "v0.9.0" 8
+get_required_kitsune_lib_repo "libKitsunemimiSakuraLang" "v0.13.0" 1
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_kitsune_lib_repo "libKitsunemimiHanamiCommon" "develop" 8
-get_required_kitsune_lib_repo "libKitsunemimiHanamiEndpoints" "develop" 1
-get_required_kitsune_lib_repo "libKitsunemimiHanamiNetwork" "develop" 8
-download_repo_github "libKitsunemimiHanamiMessages" "develop"
+get_required_kitsune_lib_repo "libKitsunemimiHanamiCommon" "v0.3.0" 8
+get_required_kitsune_lib_repo "libKitsunemimiHanamiEndpoints" "v0.2.0" 1
+get_required_kitsune_lib_repo "libKitsunemimiHanamiNetwork" "v0.5.0" 8
+download_repo_github "libKitsunemimiHanamiMessages" "v0.1.0"
 echo ""
 echo "###########################################################################################################"
 
--- src/src.pro
+++ src/src.pro
@@ -3,7 +3,7 @@ QT -= qt core gui
 TARGET = ShioriArchive
 CONFIG += c++17
 TEMPLATE = lib
-VERSION = 0.2.0
+VERSION = 0.3.0
 
 LIBS += -L../../libKitsunemimiCommon/src -lKitsunemimiCommon
 LIBS += -L../../libKitsunemimiCommon/src/debug -lKitsunemimiCommon
